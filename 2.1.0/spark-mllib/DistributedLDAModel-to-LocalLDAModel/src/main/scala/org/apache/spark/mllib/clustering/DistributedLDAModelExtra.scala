8b350cdd194ca5ee55932f8b4c971214e81f2404
Ricardo Barona <ricardo.barona@intel.com>
2017-07-24 12:09:05 -0500

    Added scaladocs.

--- 2.1.0/spark-mllib/DistributedLDAModel-to-LocalLDAModel/src/main/scala/org/apache/spark/mllib/clustering/DistributedLDAModelExtra.scala
+++ 2.1.0/spark-mllib/DistributedLDAModel-to-LocalLDAModel/src/main/scala/org/apache/spark/mllib/clustering/DistributedLDAModelExtra.scala
@@ -2,9 +2,26 @@ package org.apache.spark.mllib.clustering
 
 import org.apache.spark.mllib.linalg.Vectors
 
+/**
+  * mllib.clustering implicits
+  */
 package object implicits {
 
+  /**
+    * Extra methods for DistributedLDAModel
+    *
+    * @param distributedLDAModel Distributed LDA Model
+    */
   implicit class DistributedLDAModelExtras(val distributedLDAModel: DistributedLDAModel) {
+
+    /**
+      * Convert DistributedLDAModel to LocalLDAModel with new document concentration and topic concentration for
+      * better results.
+      *
+      * @param alpha Document concentration, can be >= 0.
+      * @param beta  Document concentration, can be >= 0.
+      * @return LocalLDAModel with given alpha and beta
+      */
     def toLocal(alpha: Double, beta: Double): LocalLDAModel = {
       new LocalLDAModel(distributedLDAModel.topicsMatrix,
         Vectors.dense(Array.fill(distributedLDAModel.k)(alpha)),
